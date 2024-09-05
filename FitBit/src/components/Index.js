b8cb2a9e187ace036142b8340bfec56eeb3cc1cd
Sruti Patro <patrocorgi29@gmail.com>
2024-09-06 00:11:34 +0530

    Initial commit

---
+++ FitBit/src/components/Index.js
@@ -0,0 +1,7 @@
+import Header from "./Header/Header";
+import Home from "./homes/Home";
+import About from "./About/About";
+import Contact from "./Contact us/Contact";
+import Footer from "./Footer/Footer";
+
+export {Home, About, Contact, Footer, Header}
